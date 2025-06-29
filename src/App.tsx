9c4a6228f45e9e71b7057a241c22aaa2da4b7f71
Annie Huang <annie.hn@gmail.com>
2025-06-29 16:39:35 +1000

    Copy initial template across from Author

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Grid } from '@mui/material';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container>
+      <Grid size={{ xs: 12 }}>
+        <Header />
+      </Grid>
+      <Grid size={{ xs: 12 }}>
+        <Container maxWidth='md'>
+          <div style={{ marginBlock: '40px 64px' }}> </div>
+        </Container>
+      </Grid>
+    </Grid>
   );
 }
 
