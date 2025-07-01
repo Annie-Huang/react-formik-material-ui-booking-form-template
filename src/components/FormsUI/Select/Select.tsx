86a591c0fafef8502321d39d60432d710e280e83
Annie Huang <annie.hn@gmail.com>
2025-07-01 21:42:15 +1000

    Add onChange event props

--- src/components/FormsUI/Select/Select.tsx
+++ src/components/FormsUI/Select/Select.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { TextField as MuiTextField } from '@mui/material';
 import { TextFieldProps } from '@mui/material/TextField/TextField';
+import { InputProps as StandardInputProps } from '@mui/material/Input/Input';
 
 export const Select = () => {
+  // const handleChange = (evt: StandardInputProps['onChange']) => {};
+  const handleChange = (
+    evt: ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>,
+  ) => {};
+
   const configSelect: TextFieldProps = {
     // ...field,
     // ...otherProps,
     select: true,
     variant: 'outlined',
     fullWidth: true,
-    // onChange: handleChange,
+    onChange: handleChange,
   };
 
   return <MuiTextField {...configSelect}></MuiTextField>;
