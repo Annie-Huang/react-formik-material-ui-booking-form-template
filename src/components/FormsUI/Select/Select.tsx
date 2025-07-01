88aad2e6190a2d4601297157ef6d4154ef6b412f
Annie Huang <annie.hn@gmail.com>
2025-07-01 22:02:56 +1000

    Tried to create a properties type for our Select component

--- src/components/FormsUI/Select/Select.tsx
+++ src/components/FormsUI/Select/Select.tsx
@@ -1,9 +1,14 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, FC } from 'react';
 import { TextField as MuiTextField } from '@mui/material';
 import { TextFieldProps } from '@mui/material/TextField/TextField';
 import { InputProps as StandardInputProps } from '@mui/material/Input/Input';
 
-export const Select = () => {
+type Options = {
+  [field: string]: string;
+};
+type SelectProps = TextFieldProps & { options: Options };
+
+export const Select: FC<SelectProps> = ({ name, options, ...otherProps }) => {
   // const handleChange = (evt: StandardInputProps['onChange']) => {};
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {};
 
