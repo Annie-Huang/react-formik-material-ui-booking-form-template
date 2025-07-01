343be74d658045e8cfecb575d0d7e2b23b034b98
Annie Huang <annie.hn@gmail.com>
2025-07-01 21:48:46 +1000

    Fix changeEvent typo

--- src/components/FormsUI/Select/Select.tsx
+++ src/components/FormsUI/Select/Select.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { TextField as MuiTextField } from '@mui/material';
 import { TextFieldProps } from '@mui/material/TextField/TextField';
 import { InputProps as StandardInputProps } from '@mui/material/Input/Input';
 
 export const Select = () => {
   // const handleChange = (evt: StandardInputProps['onChange']) => {};
-  const handleChange = (
-    evt: ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>,
-  ) => {};
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {};
 
   const configSelect: TextFieldProps = {
     // ...field,
