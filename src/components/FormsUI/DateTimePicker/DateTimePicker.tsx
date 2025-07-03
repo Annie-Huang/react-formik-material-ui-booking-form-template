b77fdb34a13f96be5a02c7a3d95ea16bee37f8c0
Annie Huang <annie.hn@gmail.com>
2025-07-03 22:09:54 +1000

    Create a basic DateTimePicker.tsx component

---
+++ src/components/FormsUI/DateTimePicker/DateTimePicker.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { TextField as MuiTextField } from '@mui/material';
+import { TextFieldProps } from '@mui/material/TextField/TextField';
+
+export const DateTimePicker = () => {
+  const configDateTimePicker: TextFieldProps = {
+    // ...field,
+    // ...otherProps,
+    type: 'date', // most important one...
+    variant: 'outlined',
+    fullWidth: true,
+    InputLabelProps: {
+      shrink: true, // most important.
+    },
+  };
+
+  return <MuiTextField {...configDateTimePicker} />;
+};
