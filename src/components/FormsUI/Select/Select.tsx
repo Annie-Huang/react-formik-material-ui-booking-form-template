15d536f935616d73f2110372c94e00eaafc496ca
Annie Huang <annie.hn@gmail.com>
2025-07-03 21:20:06 +1000

    Use setFieldValue from formik context to set value on Select name

--- src/components/FormsUI/Select/Select.tsx
+++ src/components/FormsUI/Select/Select.tsx
@@ -2,7 +2,7 @@ import React, { ChangeEvent, FC } from 'react';
 import { TextField as MuiTextField } from '@mui/material';
 import { TextFieldProps } from '@mui/material/TextField/TextField';
 import { InputProps as StandardInputProps } from '@mui/material/Input/Input';
-import { useField } from 'formik';
+import { useField, useFormikContext } from 'formik';
 
 type Options = {
   [field: string]: string;
@@ -10,11 +10,16 @@ type Options = {
 type SelectProps = TextFieldProps & { options: Options };
 
 export const Select: FC<SelectProps> = ({ name, options, ...otherProps }) => {
+  const { setFieldValue } = useFormikContext();
+
   const [field, meta] = useField(name);
   console.log('field=', field);
 
   // const handleChange = (evt: StandardInputProps['onChange']) => {};
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {};
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setFieldValue(name ?? '', value);
+  };
 
   const configSelect: TextFieldProps = {
     ...field,
