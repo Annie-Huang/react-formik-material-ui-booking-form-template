aefa3057fce816466f15bf01e81e2963d3752656
Annie Huang <annie.hn@gmail.com>
2025-07-03 22:18:07 +1000

    Get field object from useField from formik and pass it into <MiuTextField> together with otherProps

--- src/components/FormsUI/DateTimePicker/DateTimePicker.tsx
+++ src/components/FormsUI/DateTimePicker/DateTimePicker.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react';
 import { TextField as MuiTextField } from '@mui/material';
 import { TextFieldProps } from '@mui/material/TextField/TextField';
+import { useField } from 'formik';
 
 export const DateTimePicker: FC<TextFieldProps> = ({ name, ...otherProps }) => {
+  const [field, meta] = useField(name);
+
   const configDateTimePicker: TextFieldProps = {
-    // ...field,
-    // ...otherProps,
+    ...field,
+    ...otherProps,
     type: 'date', // most important one...
     variant: 'outlined',
     fullWidth: true,
